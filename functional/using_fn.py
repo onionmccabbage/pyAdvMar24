119313d6084fb22dd3811ccf86b9028d5a96c18a
onionmccabbage <onionmccabbage@live.com>
2024-03-04 14:37:57 +0000

    using functions, map, filter and reduce

--- functional/using_fn.py
+++ functional/using_fn.py
@@ -1,3 +1,6 @@
+# to access 'reduce' we import functools
+import functools
+
 
 def isOdd(n):
     '''return True if odd False if even'''
@@ -14,4 +17,25 @@ if __name__ == '__main__':
     print( isOdd(8) ) # False
     print( addThem(3,4)) # 7
     # we can use our simple functions to apply to many values
-    
+    results = map(isOdd, range(0,11) ) # range(start, stop-before, step)
+    print(results) # we have a map object - NOT the results of every function call
+    # to use a map object, we call the __next__() value
+    print( results.__next__() ) # False (0)
+    print( results.__next__() ) # True  (1) - it only runs the fn when the __next__ is called
+    print( results.__next__() ) # False (2)
+    print( results.__next__() ) # True  (3)
+    # we can access all the results by putting them into a tuple
+    results_t = tuple( results ) # only takes the remaining values from the map object
+    print(results_t)
+
+    # we can also use filter
+    odds = filter(isOdd, range(-7, 8))
+    print(odds) # a filter object. Again, the values will only be calculated on first use
+    print(odds.__next__()) # -7
+    print(odds.__next__()) # -5
+    print(odds.__next__()) # -3
+    # we can choose to iterateover a map object or a filter object
+    for i in odds: # this will iterate over all remaining filter values
+        print(i)
+
+# we also have 'reduce'
