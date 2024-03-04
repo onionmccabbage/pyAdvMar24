32ab709387b79fa73ae4d54a3dcd8b2e73ad05ab
onionmccabbage <onionmccabbage@live.com>
2024-03-04 09:25:21 +0000

    using @property

--- my_figure.py
+++ my_figure.py
@@ -9,8 +9,18 @@ class Figure:
     # NB every function within a class MUST receive 'self'
     def __init__(self, num_sides, size, colour='grey'): # like a constructor
         self.sides = num_sides # set some properties of this class
-        self.size  = size
+        self.size  = size # self.size will call the setter method
     # write methods to get and set the properties (inc validation)
+    @property # we declare this method to behave as a property
+    def size(self): # here is the 'getter' method for 'size'
+        return self.__size
+    @size.setter
+    def size(self, new_size): # here is the 'setter' method for size
+        if type(new_size) in (int, float):
+            self.__size = new_size # double-underscore is called 'name mangling'
+        else:
+            self.__size = 4 # a sensible default
+
     def set_num_sides(self, new_num_sides):
         if type(new_num_sides)==int or type(new_num_sides)==float:
             self.sides = new_num_sides
@@ -19,7 +29,7 @@ class Figure:
     # we can override built in features of Python
     def __str__(self):
         '''the __str__ method will be used whenever this class is printed'''
-        return f'The figure has {self.sides} and size is {self.size}'
+        return f'The figure has {self.sides} and size is {self.size}' # access the size getter
 
 # usually in Python we use the following structure:
 if __name__ == '__main__':
