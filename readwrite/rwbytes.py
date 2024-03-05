e2328acd91d6d4488371260449a8c0de26f990d2
onionmccabbage <onionmccabbage@live.com>
2024-03-05 09:18:09 +0000

    using json

--- readwrite/rwbytes.py
+++ readwrite/rwbytes.py
@@ -7,11 +7,16 @@ def makeBytes(values):
 def writeBytes(b):
     '''commit bytes to a persistent file'''
     try:
-        fout = open('my_bytes', 'wb') # 'w' write 'b' bytes
+        fout = open('my_bytes', 'rb') # 'w' write 'b' bytes
         fout.write(b)
-        fout.close()
     except Exception as err:
         print(err)
+    finally:
+        try:
+            fout.close() # always close - butr be careful, fout might not exist here
+        except Exception as err:
+            print(err)
+
 
 def readBytes():
     '''retreive bytes from a file'''
@@ -23,9 +28,16 @@ def readBytes():
     except Exception as err:
         print(err)
 
+def decodeBytes(b):
+    txt = (b) # there is also an encode
+    return txt
+
 if __name__ == '__main__':
     v = range(0,256)
     b = makeBytes(v) # convert the first 256 text characters into bytes
     writeBytes(b) # commit our data to a persistent byte file
     z = readBytes
     print(b) # this is still bytes
+    # can we decode the bytes
+    t = decodeBytes(b)
+    print(type(t), t)
