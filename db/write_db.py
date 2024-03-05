158eba6494246194a7c9d8172aa687b0e2937f17
onionmccabbage <onionmccabbage@live.com>
2024-03-05 10:41:57 +0000

    write one entry to database

--- db/write_db.py
+++ db/write_db.py
@@ -1,1 +1,23 @@
 '''write a single entry to our database'''
+
+import sqlite3
+
+def writeDB():
+    '''populate the DB with one animal'''
+    conn = sqlite3.connect('my_db')
+    curs = conn.cursor()
+    # sql statement (use double quotes)
+    st = '''
+    INSERT INTO zoo
+    VALUES ("Penguin", 16, 0.52)
+    '''
+    try:
+        curs.execute(st)
+        conn.commit()
+        conn.close()
+    except Exception as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    writeDB()
