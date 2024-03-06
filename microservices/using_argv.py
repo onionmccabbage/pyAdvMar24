fd63dd124ece4ae511af528dbf52ed4a37c36996
onionmccabbage <onionmccabbage@live.com>
2024-03-06 08:34:32 +0000

    using sys

---
+++ microservices/using_argv.py
@@ -0,0 +1,5 @@
+import sys
+# sys.argv is any environemnt arguments passed in to the running module
+# sys.argv[0] is always the name of the module
+# any arguments passed in will be string values
+print(sys.argv[0], sys.argv[1])
---
+++ microservices/using_sys.py
@@ -0,0 +1,7 @@
+import sys
+# we can investigate the platform on which python is running
+# especially useful if we run on a different platform to where we develop
+# maxsize tells us the largest memory-resident object we may use
+print(sys.path, sys.maxsize, sys.version_info)
+
+print(10**100000000)
