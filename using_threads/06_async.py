be278c9915e4d7cb33f135dca9f54b3995793aca
onionmccabbage <onionmccabbage@live.com>
2024-03-07 10:46:51 +0000

    using async-await

---
+++ using_threads/06_async.py
@@ -0,0 +1,16 @@
+# see https://docs.python.org/3/library/asyncio.html
+import asyncio
+
+# any function can be asyncronous
+async def main():
+    # we may await an outcome - this is blocking
+    await asyncio.sleep(1) # ilustrate a log running process
+    print('hello')
+
+if __name__=='__main__':
+    # asyncio.run(main())
+    # or
+    with asyncio.Runner() as runner:
+        runner.run(main())
+        runner.run(main())
+    print('are we there yet')
