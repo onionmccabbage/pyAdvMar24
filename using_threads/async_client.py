12d27d08efd54d60747f5f0cb512dd5c0ab01a40
onionmccabbage <onionmccabbage@live.com>
2024-03-07 11:41:24 +0000

    async client-server microservices

--- using_threads/async_client.py
+++ using_threads/async_client.py
@@ -2,11 +2,14 @@ import asyncio
 
 async def tcp_echo_client(message):
     reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+
     print(f'Send: {message!r}')
-    writer.write(message.endcode())
+    writer.write(message.encode())
     await writer.drain()
+
     data = await reader.read(1024)
-    print(f'Received {data.decode()!r}')
+    print(f'Received: {data.decode()!r}')
+
     writer.close()
     await writer.wait_closed()
 
